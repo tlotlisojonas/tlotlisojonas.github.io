d1ffd4c00fac70e8b34cf1f564984eb0ea776796
tlotlisojonas <45184739+tlotlisojonas@users.noreply.github.com>
2019-01-08 13:12:14 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" href="../css/styles.css">
+<script src="../javascript/script.js" defer></script>
+<link rel="icon" type="/images/png" href="../images/logo.png">
+<title>UCT PROMATHS</title>
+<meta name="author" content="Designed by Jonas Tlotliso for UCT Promaths">
+<meta name="Copyright" content="Copyright tlotlisojonas.github.io 2018. All Rights Reserved.">
+</head>
+<body >
+  <header>
+    <div class="nav">
+      <nav>
+          <a href="#home">Home</a>
+          <a href="#events">Pillars</a>
+          <a href="#exec">Exec</a>
+          <a href="#about">About</a>
+          <a href="#contact">Contact</a>
+      </nav>  
+    </div>
+    <h1>UCT PROMATHS</h1>
+    <img class="header_img" src="../images/header.png">
+  </header>
+  <main>
+      <section  id="display_pic">
+          <h1>OUR FOUR PILLARS!!</h1>
+          <h4><i>In all our events we always remind our family about the purpose and the aim of Promaths, this is to ensure that Promaths maintains its meaning.</i></h4>
+          <article  class="content">
+              <div class="left-content">
+                  <h3>Connect Event</h3>
+                  <p>The first pillar is the "Connect event", here Promaths senior students formally welcome all Promaths alumni into the UCT society and get to know each other because we are from different Promaths chapters. We also provide a space where our members get to have lot of fun by playing different games and activities. Most importantly from this event we set long term goal of educating society members about what the purpose and aim of the society.</p>
+                  <br>
+                  
+                  <h3>Empower Event</h3>
+                  <p> The second pillar is the "Empower event", here we empower each other about our academics and life as a promaths family. We also aim at greating the space of meanigful dailogues for our members, this is because we want to elevate relationships between our junior students and our senior students. We also teach our family about the importance of being united at all time because together we are stronger. Like all the other events we also play games and have fun</p>
+            </div>
+              <div class="right-content">
+                  <h3>Motivate Event</h3>
+                  <p>In this event we provide all the necessary motivation to our society members to enusure that they are always on top of their game. This is because at UCT it is very difficult for students to stay motivated and we as the promaths family try all means to keep our family motivated especially when they are in need of one. We usually invite invite relevant motivational speakers to this event, people who have been on the same path so that it is easy to relate and this usually spark a very strong motivation.</p>
+                  <br>
+
+                  <h3>Support Event</h3>
+                  <p>The event is meant to motivate and support the current alumni that are at UCT, different motivational speakers are invited for this purpose.This event is usually towards the end of the year and this event to help our members distress about final exams and maintain positive attitude. We also provide emotional, mental and socail support to our members where necessary. Our graduates are usually availble during this event to share their success stories with our fellow alumni</p>
+             </div>
+          </article>
+      </section>
+      <section class="our-media">
+        <?php include './media.php' ;?>
+      </section>
+      <div class="below_media">
+       <section class="exec">
+         <h1>Our Executive:</h1>
+         <div class="exec_grid">
+        <?php include './executive.php' ;?> 
+    </div>
+      </section>
+      <section class="about">
+          <h1>About US</h1>
+          <p>Promaths is a powerful partnership between Investec and Kutlwanong Maths, Science and Technology Centre. The programme provides extra tuition in Maths and Science to high school learners who show potential in these subjects. Promaths first began in Dobsonville, Soweto over 13 years ago. UCT Promaths alumni is the support structure for promaths alumni students who are studying at UCT. In this society UCT Promaths senior students welcome high school students and help them to quickly adapt to UCT with the experience they have as senior students. Each and every year we have four main events where society members get together to celebrate our four pillars.</p>
+          <br>
+          <br>
+          <br>
+          <h3>Executive Committee</h3>
+          <p>Like every other family, a family needs parents. Our execuive committee is made of 10 members as shown on the left hand side. Our executive committee serves as the parents to our family, they ensure that all society  activies happen according to plan. Our society also has other small supporting structures such as "Mento-mentees" program and "Promaths cares". Mento-mentee program is where our senior members sign up to be mentors to our junior members and provide them with mentorship througout the year. Promaths cares is a program that we initiated to assist our members with their basic needs such as stationary and toiletries, we usually encourage people to donate anything that can be needed by the other person. As Promaths executive we usually arrange some regular meetings throughout the year just to catch up and constantly support one another.</p>
+      </section>
+    </div>
+  </main>
+  <footer>
+        <!-- Social media and contact links -->
+        <div class="contact">
+            <a class="mail" href="uctpromathsalumni@gmail.com">Email</a>
+            <a class="twitter" href="https://twitter.com/login?lang=en" target="_blank">Twitter</a>
+            <a class="fb" href="https://www.facebook.com/" target="_blank">Facebook</a>
+            <a class="ig" href="https://www.instagram.com/tlotliso_jones/?hl=en" target="_blank">Instagram</a>
+        </div>
+       <div class="developer">
+           <p><?php include './copywrite.php'; ?> | Designed by <a class="dev_link" href="tlotlisojonas.github.io" a>Tlotliso Jonas</a></p>
+       </div>
+  </footer>
+</body>
+</html>
+
